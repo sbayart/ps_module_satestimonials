29bf75b7461fe8b1644640b9c2aa113df33ab755
solene <solenantoine@gmail.com>
2017-08-02 14:30:23 +0200

    add/remove menu

--- classes/SaTestimonials.php
+++ classes/SaTestimonials.php
@@ -19,18 +19,45 @@ class SaTestimonials extends module
     {
         if (!parent::install()
             || !$this->registerHook('displayHome')
+            || !$this->installTab()
         ) {
             return false;
         }
         return true;
     }
 
+    public function installTab()
+    {
+        $tab = new Tab();
+        $tab->active = 1;
+        $tab->class_name = 'AdminTestimonials';
+        $tab->name = array();
+        foreach (Language::getLanguages(true) as $lang) {
+            $tab->name[$lang['id_lang']] = 'Mon Module de Temoignages';
+        }
+        $tab->id_parent = 0;
+        $tab->module = $this->name;
+        return $tab->add();
+    }
+
     public function uninstall()
     {
         if (!parent::uninstall()
+        || !$this->uninstallTab()
     ) {
         return false;
     }
     return true;
+    }
+
+    public function uninstallTab()
+    {
+        $id_tab = (int)Tab::getIdFromClassName('AdminTestimonials');
+        if ($id_tab) {
+            $tab = new Tab($id_tab);
+            return $tab->delete();
+        } else {
+            return false;
+        }
     }
 }
